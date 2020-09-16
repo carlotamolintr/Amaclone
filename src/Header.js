b15b38d18f7de94033eaca00d6fde97662e17b87
carlotamolintr <carlota@MacBook-Pro-de-Carlota.local>
2020-09-16 14:18:32 +0200

    nav bar aligned

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import "./Header.css"
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className="header">
-            <img className="header-logo" alt="amaclone" src="https://pngimg.com/uploads/amazon/amazon_PNG25.png" />
-            <div className="header-searcher">
-                <input className="header-search-input" type="text"></input>
+            <img className="header__logo" alt="amaclone" src="https://pngimg.com/uploads/amazon/amazon_PNG25.png" />
+            <div className="header__searcher">
+                <input className="header__search" type="text"></input>
+                <SearchIcon className="header__searchIcon"></SearchIcon>
             </div>
             <div className="header-options">
-                <span className="header-option-one">Hello Guest</span>
-                <span className="header-option-two">Sign in</span>
-                <span className="header-option-three">Returns</span>
+                <span className="header__optionOne">Hello Guest</span>
+                <span className="header__optionTwo">Sign in</span>
+                <span className="header__optionThree">Returns</span>
             </div>
         </div>
     )
